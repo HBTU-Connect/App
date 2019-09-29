4fa4342db93a72c5cfd87e7fdf4ed2c0a572e11c
Yashveer Talan <yashveertalan@gmail.com>
2019-09-30 01:17:39 +0530

    Added Welcome Page

--- src/components/welcome.js
+++ src/components/welcome.js
@@ -1,7 +1,16 @@
 import React from 'react';
 import { Field, reduxForm } from 'redux-form';
 
+//components
 import validate from './validate';
+
+//images
+import studentsImage from '../images/students.png';
+import joinImage from '../images/dive.png';
+import postImage from '../images/post.png';
+import chatImage from '../images/chatting.png';
+import askImage from '../images/ask.png';
+import clubImage from '../images/club.png'
 
 class Welcome extends React.Component{
 
@@ -58,6 +67,88 @@ class Welcome extends React.Component{
                             </div>
                         </div>
                     </div>
+                </div>
+                <div className='section_two'>
+                    <div className='section_two-content'>
+                        <img src={studentsImage} alt='students'/>
+                        <div className='section_two-heading'>
+                            <div className='section_two-heading-primary'>
+                                Connecting Mates
+                            </div>
+                            <div className='divider'></div>
+                            <div className='section_two-heading-secondary'>
+                                Join the community and Grow your connections in the College.
+                            </div>
+                        </div>
+                    </div>
+                    
+                </div>
+                <div className='section_three'>
+                    <div className='cards'>
+                        <div className= 'card-1-of-3'>
+                            <div class='card-image'>
+                                <img src={postImage} alt='post Icon' />
+                            </div>
+                            <div className='card-heading'>
+                                Add Posts
+                            </div>
+                            <div className='card-content'>
+                                Lorem ipsum is placeholder text commonly used in the graphic, print, and 
+                                publishing industries for previewing layouts and visual mockups.
+                            </div>
+                        </div>
+                        <div className='card-1-of-3'>
+                            <div class='card-image'>
+                                <img src={chatImage} alt='chat Icon' />
+                            </div>
+                            <div className='card-heading'>
+                             Chat
+                            </div>
+                            <div className='card-content'>
+                                Lorem ipsum is placeholder text commonly used in the graphic, print, and 
+                                publishing industries for previewing layouts and visual mockups.  
+                            </div>
+                        </div>
+                        <div className='card-1-of-3'>
+                            <div class='card-image'>
+                                <img src={askImage} alt='Ask Icon' />
+                            </div>
+                            <div className='card-heading'>
+                                Ask
+                            </div>
+                            <div className='card-content'>
+                                Lorem ipsum is placeholder text commonly used in the graphic, print, and 
+                                publishing industries for previewing layouts and visual mockups.
+                            </div>
+                        </div>
+                    </div>
+                </div>
+
+                <div className='section_four'>
+                    <div className='section_four-content'>
+                        <img src={clubImage} alt='club icon' />
+                        <div className='section_four-heading'>
+                            <div className='section_four-heading-primary'>
+                                Clubs
+                            </div>
+                            <div className='divider'></div>
+                            <div className='section_four-heading-secondary'>
+                                You can find all our College Clubs here. 
+                                And become A member .
+                            </div>
+                        </div>
+                    </div>
+                </div>
+
+                <div className='section_five'>
+                    <div className='section_five-content'>
+                        <img src={joinImage} alt='join' />
+                        <div className='section_five-heading'>
+                            Dive into the World of HBTU Connect.
+                            <button> Join Now </button>
+                        </div>
+                    </div> 
+
                 </div>
             </div>
         );
