534e4f6ce3efc65d7d936e04d176eb009e937d99
Yashveer Talan <yashveertalan@gmail.com>
2019-10-01 09:49:02 +0530

    Improvements in sign Up Page

--- src/components/auth/renderField.js
+++ src/components/auth/renderField.js
@@ -1,19 +1,20 @@
 import React from 'react';
 
-const renderField = ({ input, label, type, givenValue, disabled, meta: {valid, dirty, active, touched, error } }) => (
+const renderField = ({ input, label, type, input: { value }, meta: {pristine, valid, dirty, active, touched, error } }) => (
     <div className='input-field'>
         
       <div >
         <input 
-            className={ dirty || active ? (valid ? 'input-field--value touched' : 'input-field--value error') : 'input-field--value'}
+            className={ value || dirty || active ? (valid ? 'input-field--value touched' : 'input-field--value error') : 'input-field--value'}
             {...input} 
             type={type}
-            value={givenValue === ""? undefined : givenValue}
-            disabled={disabled}
+            // value={givenValue === ""? undefined : givenValue}
+            disabled={value && pristine ? true : false }
+            
         />
         {touched && error && <span className='input-field--value-error'>{error}</span>}
       </div>
-      <label className={ dirty || active ? 'input-field--label touched' : 'input-field--label'}>
+      <label className={ value || dirty || active ? 'input-field--label touched' : 'input-field--label'}>
             {label}
         </label>
     </div>
